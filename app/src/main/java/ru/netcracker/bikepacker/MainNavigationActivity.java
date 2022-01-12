83815172d4634f6021f3a7c2dbb75bd2ef1570ec
Alexander <mr.zeddd123@gmail.com>
2022-01-12 19:24:46 +0300

    Temp changes

--- app/src/main/java/ru/netcracker/bikepacker/MainNavigationActivity.java
+++ app/src/main/java/ru/netcracker/bikepacker/MainNavigationActivity.java
@@ -3,26 +3,35 @@ package ru.netcracker.bikepacker;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint;
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlay;
+import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlayOptions;
+import org.osmdroid.views.overlay.simplefastpoint.SimplePointTheme;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MapPage extends AppCompatActivity {
+public class MainNavigationActivity extends AppCompatActivity {
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView map = null;
     private MyLocationNewOverlay mLocationOverlay;
@@ -46,10 +55,10 @@ public class MapPage extends AppCompatActivity {
         //tile servers will get you banned based on this string
 
         //inflate and create the map
-        setContentView(R.layout.activity_map_page);
+        setContentView(R.layout.activity_main_navigation);
 
         map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setTileSource(TileSourceFactory.MAPNIK); //по стандарту тут стоит MAPNIK
 
         requestPermissionsIfNecessary(new String[]{
                 // if you need to show the current location, uncomment the line below
@@ -58,37 +67,80 @@ public class MapPage extends AppCompatActivity {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         });
 
-        map.setBuiltInZoomControls(true);
+        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
         map.setMultiTouchControls(true);
 
 
         IMapController mapController = map.getController();
         mapController.setZoom(9.5);
 
-//        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-//
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        GeoPoint startPoint = new GeoPoint(locationManager.getLastKnownLocation(String.valueOf(gpsMyLocationProvider)));
+        //добавление точек
+
+        GeoPoint myLocation = null;
+        if(MyLocationListener.imHere == null)
+            myLocation = new GeoPoint(51.67204, 39.1843);
+        else
+            myLocation = new GeoPoint(MyLocationListener.imHere);
+
+        // create 10k labelled points
+        // in most cases, there will be no problems of displaying >100k points, feel free to try
+        List<IGeoPoint> points = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            points.add(new LabelledGeoPoint(myLocation.getLatitude() + Math.random() * 5, myLocation.getLongitude() + Math.random() * 5
+                    , "Point #" + i));
+        }
+
+        // wrap them in a theme
+        SimplePointTheme pt = new SimplePointTheme(points, true);
+
+        // create label style
+        Paint textStyle = new Paint();
+        textStyle.setStyle(Paint.Style.FILL);
+        textStyle.setColor(Color.parseColor("#0000ff"));
+        textStyle.setTextAlign(Paint.Align.CENTER);
+        textStyle.setTextSize(24);
+
+        // set some visual options for the overlay
+        // we use here MAXIMUM_OPTIMIZATION algorithm, which works well with >100k points
+        SimpleFastPointOverlayOptions opt = SimpleFastPointOverlayOptions.getDefaultStyle()
+                .setAlgorithm(SimpleFastPointOverlayOptions.RenderingAlgorithm.MAXIMUM_OPTIMIZATION)
+                .setRadius(7).setIsClickable(true).setCellSize(15).setTextStyle(textStyle);
+
+        // create the overlay with the theme
+        final SimpleFastPointOverlay sfpo = new SimpleFastPointOverlay(pt, opt);
+
+        // onClick callback
+        sfpo.setOnClickListener(new SimpleFastPointOverlay.OnClickListener() {
+            @Override
+            public void onClick(SimpleFastPointOverlay.PointAdapter points, Integer point) {
+                Toast.makeText(map.getContext()
+                        , "You clicked " + ((LabelledGeoPoint) points.get(point)).getLabel()
+                        , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // add overlay
+        map.getOverlays().add(sfpo);
+
+        putMeOnMap();
+    }
+
+    private void putMeOnMap() {
         GeoPoint startPoint;
         if(MyLocationListener.imHere == null)
             startPoint = new GeoPoint(51.67204, 39.1843);
         else
             startPoint = new GeoPoint(MyLocationListener.imHere);
-        mapController.setCenter(startPoint);
-
-        this.mLocationOverlay = new MyLocationNewOverlay(gpsMyLocationProvider,map);
-        this.mLocationOverlay.enableMyLocation();
-        map.getOverlays().add(this.mLocationOverlay);
+        map.getController().setCenter(startPoint);
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_user_location_icon));
+        startMarker.setTitle("Start point");
+        map.getOverlays().add(startMarker);
     }
+
+
 
     @Override
     public void onResume() {
