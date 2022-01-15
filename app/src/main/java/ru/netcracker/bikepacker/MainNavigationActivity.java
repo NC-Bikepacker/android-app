4ec0a0590f72c695fb20a2e1d4a3bd7dbec56053
Alexander <mr.zeddd123@gmail.com>
2022-01-15 10:53:49 +0300

    fixed remarks

--- app/src/main/java/ru/netcracker/bikepacker/MainNavigationActivity.java
+++ app/src/main/java/ru/netcracker/bikepacker/MainNavigationActivity.java
@@ -3,65 +3,52 @@ package ru.netcracker.bikepacker;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.Paint;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-import org.osmdroid.views.overlay.simplefastpoint.LabelledGeoPoint;
-import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlay;
-import org.osmdroid.views.overlay.simplefastpoint.SimpleFastPointOverlayOptions;
-import org.osmdroid.views.overlay.simplefastpoint.SimplePointTheme;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainNavigationActivity extends AppCompatActivity {
-    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private static final double START_ZOOM = 9.5;
     private MapView map = null;
     private MyLocationNewOverlay mLocationOverlay;
+    private GeoPoint userLocation;
+    private Context ctx;
+    private LocationManager locationManager;
+    private static final int MIN_TIME_MS = 5000;
+    private static final int MIN_DISTANCE = 10;
+    private static final GeoPoint DEFAULT_POINT = new GeoPoint(51.672, 39.1843);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //handle permissions first, before map is created. not depicted here
-
-        //load/initialize the osmdroid configuration, this can be done
-        Context ctx = getApplicationContext();
+        ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         GpsMyLocationProvider gpsMyLocationProvider = new GpsMyLocationProvider(ctx);
-        MyLocationListener.SetUpLocationListener(ctx);
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
-        //tile servers will get you banned based on this string
 
-        //inflate and create the map
         setContentView(R.layout.activity_main_navigation);
 
-        map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK); //по стандарту тут стоит MAPNIK
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
 
         requestPermissionsIfNecessary(new String[]{
-                // if you need to show the current location, uncomment the line below
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 // WRITE_EXTERNAL_STORAGE is required in order to show the map
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -70,111 +57,55 @@ public class MainNavigationActivity extends AppCompatActivity {
         map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
         map.setMultiTouchControls(true);
 
-
         IMapController mapController = map.getController();
-        mapController.setZoom(9.5);
+        mapController.setZoom(START_ZOOM);
 
-        //добавление точек
+        locationManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
 
-        GeoPoint myLocation = null;
-        if(MyLocationListener.imHere == null)
-            myLocation = new GeoPoint(51.67204, 39.1843);
-        else
-            myLocation = new GeoPoint(MyLocationListener.imHere);
-
-        // create 10k labelled points
-        // in most cases, there will be no problems of displaying >100k points, feel free to try
-        List<IGeoPoint> points = new ArrayList<>();
-        for (int i = 0; i < 10000; i++) {
-            points.add(new LabelledGeoPoint(myLocation.getLatitude() + Math.random() * 5, myLocation.getLongitude() + Math.random() * 5
-                    , "Point #" + i));
+        if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ctx, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //TODO:возвращение на экран с авторизацией
+            return;
         }
 
-        // wrap them in a theme
-        SimplePointTheme pt = new SimplePointTheme(points, true);
+        Location imHere = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        // create label style
-        Paint textStyle = new Paint();
-        textStyle.setStyle(Paint.Style.FILL);
-        textStyle.setColor(Color.parseColor("#0000ff"));
-        textStyle.setTextAlign(Paint.Align.CENTER);
-        textStyle.setTextSize(24);
+        userLocation = imHere != null ? new GeoPoint(imHere) : DEFAULT_POINT;
 
-        // set some visual options for the overlay
-        // we use here MAXIMUM_OPTIMIZATION algorithm, which works well with >100k points
-        SimpleFastPointOverlayOptions opt = SimpleFastPointOverlayOptions.getDefaultStyle()
-                .setAlgorithm(SimpleFastPointOverlayOptions.RenderingAlgorithm.MAXIMUM_OPTIMIZATION)
-                .setRadius(7).setIsClickable(true).setCellSize(15).setTextStyle(textStyle);
+        mapController.setCenter(userLocation);
 
-        // create the overlay with the theme
-        final SimpleFastPointOverlay sfpo = new SimpleFastPointOverlay(pt, opt);
-
-        // onClick callback
-        sfpo.setOnClickListener(new SimpleFastPointOverlay.OnClickListener() {
-            @Override
-            public void onClick(SimpleFastPointOverlay.PointAdapter points, Integer point) {
-                Toast.makeText(map.getContext()
-                        , "You clicked " + ((LabelledGeoPoint) points.get(point)).getLabel()
-                        , Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // add overlay
-        map.getOverlays().add(sfpo);
-
-        putMeOnMap();
+        this.mLocationOverlay = new MyLocationNewOverlay(gpsMyLocationProvider, map);
+        this.mLocationOverlay.enableMyLocation();
+        map.getOverlays().add(this.mLocationOverlay);
     }
-
-    private void putMeOnMap() {
-        GeoPoint startPoint;
-        if(MyLocationListener.imHere == null)
-            startPoint = new GeoPoint(51.67204, 39.1843);
-        else
-            startPoint = new GeoPoint(MyLocationListener.imHere);
-        map.getController().setCenter(startPoint);
-        Marker startMarker = new Marker(map);
-        startMarker.setPosition(startPoint);
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_user_location_icon));
-        startMarker.setTitle("Start point");
-        map.getOverlays().add(startMarker);
-    }
-
-
 
     @Override
     public void onResume() {
         super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (int i = 0; i < grantResults.length; i++) {
-            permissionsToRequest.add(permissions[i]);
-        }
-        if (permissionsToRequest.size() > 0) {
+        String[] permissionsToRequest = permissions.clone();
+        if (permissionsToRequest.length > 0) {
             ActivityCompat.requestPermissions(
                     this,
-                    permissionsToRequest.toArray(new String[0]),
+                    permissionsToRequest,
                     REQUEST_PERMISSIONS_REQUEST_CODE);
         }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //TODO:возвращение на экран с авторизацией
+            return;
+        }
+        userLocation = new GeoPoint(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+        map.getController().setCenter(userLocation);
     }
 
     private void requestPermissionsIfNecessary(String[] permissions) {
