0f7099ebf4e2e0dc7f86484d9e7fca195ae789cb
san-vrn <sanvrn036@gmail.com>
2022-05-08 01:57:56 +0300

    [DEV/show-friend-tracks] - edit date padding in account editor

--- app/src/main/java/ru/netcracker/bikepacker/view/AccountEditorFragment.java
+++ app/src/main/java/ru/netcracker/bikepacker/view/AccountEditorFragment.java
@@ -2,6 +2,7 @@ package ru.netcracker.bikepacker.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -15,9 +16,8 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
-import java.util.zip.Inflater;
+import java.util.Objects;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -65,92 +65,86 @@ public class AccountEditorFragment extends Fragment {
         email.setText(userAccountManager.getUser().getEmail());
         username.setText(userAccountManager.getUser().getUsername());
 
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                user = new SignUpModel();
-                //флаг изменений, если есть изменения, то true
-                boolean editFlag = false;
+        save.setOnClickListener(view -> {
+            user = new SignUpModel();
+            //флаг изменений, если есть изменения, то true
+            boolean editFlag;
 
-                //проверка, что поле в фрагменте изменено и не является нулевым
-                if(!userAccountManager.getUser().getFirstname().equals(firstname.getText().toString()) && !firstname.getText().toString().isEmpty()){ editFlag=true; }
-                if(!userAccountManager.getUser().getLastname().equals(lastname.getText().toString())&& !lastname.getText().toString().isEmpty()){ editFlag=true;}
-                if(!userAccountManager.getUser().getEmail().equals(email.getText().toString()) && !email.getText().toString().isEmpty()){editFlag=true;}
-                if(!userAccountManager.getUser().getUsername().equals(username.getText().toString()) && !username.getText().toString().isEmpty()){editFlag=true;}
+            //проверка, что поле в фрагменте изменено и не является нулевым
+            if(!userAccountManager.getUser().getFirstname().equals(Objects.requireNonNull(firstname.getText()).toString()) && !firstname.getText().toString().isEmpty()){ editFlag=true; }
+            if(!userAccountManager.getUser().getLastname().equals(Objects.requireNonNull(lastname.getText()).toString())&& !lastname.getText().toString().isEmpty()){ editFlag=true;}
+            if(!userAccountManager.getUser().getEmail().equals(Objects.requireNonNull(email.getText()).toString()) && !email.getText().toString().isEmpty()){editFlag=true;}
+            if(!userAccountManager.getUser().getUsername().equals(Objects.requireNonNull(username.getText()).toString()) && !username.getText().toString().isEmpty()){editFlag=true;}
 
 
-                //устанавливаем поля для новых данных юзера
-                user.setFirstname(firstname.getText().toString());
-                user.setLastname(lastname.getText().toString());
-                user.setEmail(email.getText().toString());
-                user.setUsername(username.getText().toString());
-                if(password.getText().toString().length() >= 8 &&
-                        password.getText().toString().equals(confirmPassword.getText().toString()) &&
-                        PasswordGeneratingService.isValidPassword(password.getText().toString())){
-                    user.setPassword(password.getText().toString());
-                    editFlag = true;
+            //устанавливаем поля для новых данных юзера
+            user.setFirstname(firstname.getText().toString());
+            user.setLastname(lastname.getText().toString());
+            user.setEmail(email.getText().toString());
+            user.setUsername(username.getText().toString());
+            if(Objects.requireNonNull(password.getText()).toString().length() >= 8 &&
+                    password.getText().toString().equals(Objects.requireNonNull(confirmPassword.getText()).toString()) &&
+                    PasswordGeneratingService.isValidPassword(password.getText().toString())){
+                user.setPassword(password.getText().toString());
+                editFlag = true;
+            }
+            else {
+                editFlag = false;
+                if(!password.getText().toString().equals(Objects.requireNonNull(confirmPassword.getText()).toString())){
+                    Toast.makeText(getContext(), "введенные пароли не совпадают, повторите попытку ввода", Toast.LENGTH_LONG).show();
+                    password.setText("");
+                    confirmPassword.setText("");
+                    return;
                 }
-                else {
-                    editFlag = false;
-                    if(!password.getText().toString().equals(confirmPassword.getText().toString())){
-                        Toast.makeText(getContext(), "введенные пароли не совпадают, повторите попытку ввода", Toast.LENGTH_LONG).show();
-                        password.setText("");
-                        confirmPassword.setText("");
-                        return;
-                    }
-                    if(password.getText().toString().length() < 8){
-                        Toast.makeText(getContext(), "длинна пароля менее 8 символов, введите пароль удовлетворяющий условиям безопасности", Toast.LENGTH_LONG).show();
-                        password.setText("");
-                        confirmPassword.setText("");
-                        return;
-                    }
-                    if(!PasswordGeneratingService.isValidPassword(password.getText().toString())){
-                        Toast.makeText(getContext(), "введенный пароль не соответствует условиям безопасности, введите пароль удовлетворяющий условиям безопасности", Toast.LENGTH_LONG).show();
-                        password.setText("");
-                        confirmPassword.setText("");
-                        return;
-                    }
+                if(password.getText().toString().length() < 8){
+                    Toast.makeText(getContext(), "длинна пароля менее 8 символов, введите пароль удовлетворяющий условиям безопасности", Toast.LENGTH_LONG).show();
+                    password.setText("");
+                    confirmPassword.setText("");
+                    return;
                 }
+                if(!PasswordGeneratingService.isValidPassword(password.getText().toString())){
+                    Toast.makeText(getContext(), "введенный пароль не соответствует условиям безопасности, введите пароль содержащий хотя бы одну цифру, одну заглавную букву и одну строчную букву", Toast.LENGTH_LONG).show();
+                    password.setText("");
+                    confirmPassword.setText("");
+                    return;
+                }
+            }
 
-                if (editFlag ==true){
-                    RetrofitManager.getInstance(getContext())
-                            .getJSONApi()
-                            .updateUserData(userAccountManager.getCookie(), user)
-                            .enqueue(new Callback<UserModel>() {
-                                @Override
-                                public void onResponse(Call<UserModel> call, Response<UserModel> response) {
-                                    if (response.isSuccessful()){
-                                        SessionManager.getInstance(getContext())
-                                                .setSessionUser(response.body());
-                                        Toast.makeText(getContext(), "Данные аккаунта успешно обновлены", Toast.LENGTH_SHORT).show();
-                                    }
-                                    else {
-                                        Log.e("AccountEditorFragment.class", "Не выполнен запрос отправки данных аккаунта клиента");
-                                        Toast.makeText(getContext(), "Проверьте соединение интернет.", Toast.LENGTH_SHORT).show();
-                                    }
+            if (editFlag){
+                RetrofitManager.getInstance(getContext())
+                        .getJSONApi()
+                        .updateUserData(userAccountManager.getCookie(), user)
+                        .enqueue(new Callback<UserModel>() {
+                            @Override
+                            public void onResponse(@NonNull Call<UserModel> call, @NonNull Response<UserModel> response) {
+                                if (response.isSuccessful() && response.body()!=null){
+                                    SessionManager.getInstance(getContext())
+                                            .setSessionUser(response.body());
+                                    Toast.makeText(getContext(), "Данные аккаунта успешно обновлены", Toast.LENGTH_SHORT).show();
                                 }
-
-                                @Override
-                                public void onFailure(Call<UserModel> call, Throwable t) {
-                                    Log.e("AccountEditorFragment.class", "Ошибка выполнения запроса отправки данных аккаунта клиента");
-                                    Toast.makeText(getContext(), "Ошибка запроса отправки данных", Toast.LENGTH_SHORT).show();
+                                else {
+                                    Log.e("AccountEditorFragment.class", "Не выполнен запрос отправки данных аккаунта клиента");
+                                    Toast.makeText(getContext(), "Проверьте соединение интернет.", Toast.LENGTH_SHORT).show();
                                 }
-                            });
-                }
+                            }
+
+                            @Override
+                            public void onFailure(@NonNull Call<UserModel> call, @NonNull Throwable t) {
+                                Log.e("AccountEditorFragment.class", "Ошибка выполнения запроса отправки данных аккаунта клиента");
+                                Toast.makeText(getContext(), "Ошибка запроса отправки данных", Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
-        backFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                Fragment activeFragment = MainNavigationActivity.Companion.getActiveFragment();
-                fragmentManager.beginTransaction()
-                        .hide(activeFragment)
-                        .show(fragmentManager.findFragmentByTag("user_menu"))
-                        .commit();
-                MainNavigationActivity.Companion.setActiveFragment(fragmentManager.findFragmentByTag("user_menu"));
-            }
+        backFragment.setOnClickListener(view -> {
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            Fragment activeFragment = Objects.requireNonNull(MainNavigationActivity.Companion.getActiveFragment());
+            fragmentManager.beginTransaction()
+                    .hide(activeFragment)
+                    .show(Objects.requireNonNull(fragmentManager.findFragmentByTag("user_menu")))
+                    .commit();
+            MainNavigationActivity.Companion.setActiveFragment(fragmentManager.findFragmentByTag("user_menu"));
         });
         return viewAccountEditor;
     }
